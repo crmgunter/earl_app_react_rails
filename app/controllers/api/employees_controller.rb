e69efd8b5a457aeeab012892559283973360c999
crmgunter <crmgunter@gmail.com>
2018-05-21 00:16:54 -0400

    got employee controller set up with what i think i basically need for now. don't think there's any use in update for the time being. might add it later but time will tell

--- app/controllers/api/employees_controller.rb
+++ app/controllers/api/employees_controller.rb
@@ -1,2 +1,37 @@
 class Api::EmployeesController < ApplicationController
+
+    def index
+        @shift = Shift.find(params[:shift_id])
+        @employees = @shift.employees
+
+        render json: @employees
+    end
+
+    def show
+        @shift = Shift.find(params[:shift_id])
+        @employee = @shift.employees.find(params[:id])
+
+        render json: @employee
+    end
+
+    def create
+        @shift = Shift.find(params[:shift_id])
+        @new_employee = @shift.employees.create!(employee_params)
+        
+        render json: {
+            employee: @new_employee
+        }
+    end
+
+    def destroy
+        @shift = Shift.find(params[:shift_id])
+        @employee = @shift.employees.find(params[:id]).delete
+
+        render status: :ok
+    end
+
+    def employee_params
+        params.require(:employee).permit(:name, :position, :phone, :photo, :shift_id)
+    end
+
 end
